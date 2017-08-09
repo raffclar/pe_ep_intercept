6f125d5dd49f713d64f2df2a0db7f5a4cb5f66d1
Gavxn <gavxn@zanidrak.com>
2017-08-09 02:49:52 +0100

    Static PePatch functions are removed. Now scoped to the implementation file

--- PePatch.hpp
+++ PePatch.hpp
@@ -21,14 +21,10 @@ private:
     IMAGE_FILE_HEADER file_header;
     IMAGE_OPTIONAL_HEADER optional_header;
 	std::vector<IMAGE_SECTION_HEADER> section_headers;
-
-    static uint32_t Align(uint32_t number, uint32_t multiple);
-    static bool ReplaceDword(std::vector<char> code_buffer, uint32_t target_value, uint32_t replacement_value);
-    static std::vector<char> CopyBytes(char *byte_start, char *byte_end);
 public:
     explicit PePatch(std::string path);
 	std::string CreateEntryPointSubroutine(uint32_t original_entry_point);
     std::vector<char> Assemble(const std::string &assembly);
     void AddSection(const std::string &new_section_name, uint32_t code_size);
-    void SaveFile(std::string new_path, std::vector<char> subroutine_buffer);
+    void SaveFile(std::string new_path, std::vector<char> code_buffer);
 };
