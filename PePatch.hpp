036fcf859d9ec85aaebbcd74336f48cf5c102ea5
Gavxn <gavxn@zanidrak.com>
2017-08-09 03:26:15 +0100

    Replace tabs with spaces

--- PePatch.hpp
+++ PePatch.hpp
@@ -13,17 +13,17 @@
 class PePatch {
 private:
     std::string path;
-	std::fstream file_input;
-	std::vector<char> file_buffer;
+    std::fstream file_input;
+    std::vector<char> file_buffer;
 
     uint32_t nt_header_signature;
     IMAGE_DOS_HEADER dos_header;
     IMAGE_FILE_HEADER file_header;
     IMAGE_OPTIONAL_HEADER optional_header;
-	std::vector<IMAGE_SECTION_HEADER> section_headers;
+    std::vector<IMAGE_SECTION_HEADER> section_headers;
 public:
     explicit PePatch(std::string path);
-	std::string CreateEntryPointSubroutine(uint32_t original_entry_point);
+    std::string CreateEntryPointSubroutine(uint32_t original_entry_point);
     std::vector<char> Assemble(const std::string &assembly);
     void AddSection(const std::string &new_section_name, uint32_t code_size);
     void SaveFile(std::string new_path, std::vector<char> code_buffer);
