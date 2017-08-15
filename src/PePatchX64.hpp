bb717d4f8811f7edc87051207105cc12b8370135
Gavxn <gavxn@zanidrak.com>
2017-08-15 17:09:32 +0100

    Create derived PePatchX64 and PePatchX86

--- src/PePatchX64.hpp
+++ src/PePatchX64.hpp
@@ -5,10 +5,13 @@
 #ifndef PE_EP_INTERCEPT_PEPATCHX64_HPP
 #define PE_EP_INTERCEPT_PEPATCHX64_HPP
 
+#include "PePatch.hpp"
 
-class PePatchX64 {
-
-};
+namespace PeEpIntercept {
+    class PePatchX64 : public PePatch {
+        explicit PePatchX64(std::string &path) : PePatch(path) {}
+    };
+}
 
 
 #endif //PE_EP_INTERCEPT_PEPATCHX64_HPP
--- src/PePatchX86.hpp
+++ src/PePatchX86.hpp
@@ -5,10 +5,13 @@
 #ifndef PE_EP_INTERCEPT_PEPATCHX86_HPP
 #define PE_EP_INTERCEPT_PEPATCHX86_HPP
 
+#include "PePatch.hpp"
 
-class PePatchX86 {
-
-};
+namespace PeEpIntercept {
+    class PePatchX86 : public PePatch {
+        explicit PePatchX86(std::string &path) : PePatch(path) {}
+    };
+}
 
 
 #endif //PE_EP_INTERCEPT_PEPATCHX86_HPP
