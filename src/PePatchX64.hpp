329e03354945fbbdfa049577113a8a6e57ab18a7
Gavxn <gavxn@zanidrak.com>
2017-11-26 20:47:29 +0000

    Remove author comments

--- src/PePatchX64.hpp
+++ src/PePatchX64.hpp
@@ -1,7 +1,3 @@
-//
-// Created by gavxn on 15/08/2017.
-//
-
 #ifndef PE_EP_INTERCEPT_PEPATCHX64_HPP
 #define PE_EP_INTERCEPT_PEPATCHX64_HPP
 
--- src/PePatchX86.hpp
+++ src/PePatchX86.hpp
@@ -1,7 +1,3 @@
-//
-// Created by gavxn on 15/08/2017.
-//
-
 #ifndef PE_EP_INTERCEPT_PEPATCHX86_HPP
 #define PE_EP_INTERCEPT_PEPATCHX86_HPP
 
