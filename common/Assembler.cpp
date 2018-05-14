e36a47c3cc08fd635221f22a0a562a4c8a17fbec
Gavxn <gavxn@zanidrak.com>
2018-05-14 13:27:32 +0100

    Refs #overwrite-fix, Clean up the Assembler class

--- common/Assembler.cpp
+++ common/Assembler.cpp
@@ -2,7 +2,7 @@
 #include "Assembler.hpp"
 
 namespace Interceptor {
-    std::vector<char> Assembler::assemble(PeArch type, const std::string &assembly) {
+    std::vector<char> Assembler::assemble(Architecture type, const std::string &assembly) {
         std::vector<char> instructions;
 
         if (assembly.empty()) {
@@ -25,35 +25,34 @@ namespace Interceptor {
         ks_mode instruct_mode;
 
         switch (type) {
-            case PeArch::x86:
+            case Architecture::x86:
                 instruct_mode = KS_MODE_32;
                 break;
-            case PeArch::x64:
+            case Architecture::x64:
                 instruct_mode = KS_MODE_64;
                 break;
             default:
-                throw std::runtime_error("executable type not supported");
+                throw std::runtime_error("Executable type not supported");
         }
 
         if (ks_open(KS_ARCH_X86, instruct_mode, &ks) != KS_ERR_OK) {
-            throw std::runtime_error("failed to open keystone");
+            throw std::runtime_error("Failed to open keystone");
         }
 
-        std::unique_ptr<ks_engine[],
-                decltype(ks_deleter)> ks_ptr(ks, ks_deleter);
+        std::unique_ptr<ks_engine[], decltype(ks_deleter)> ks_ptr(ks, ks_deleter);
 
         if (ks_asm(ks, assembly.c_str(), 0, &encode, &size, &count) != KS_ERR_OK) {
-            throw std::runtime_error("failed to assemble instructions");
+            throw std::runtime_error("Failed to assemble instructions");
         }
 
         std::unique_ptr<unsigned char[],
                 decltype(code_deleter)> encode_ptr(encode, code_deleter);
 
-        if (size > 0xffffffff) {
-            throw std::runtime_error("exceeded max section size");
+        if (size > sizeof(uint32_t) * 0xff) {
+            throw std::runtime_error("Exceeded max section size");
         }
 
-        for (size_t i = 0; i < size; i++) {
+        for (auto i = 0; i < size; i++) {
             auto encoded = static_cast<char>(encode[i]);
             instructions.push_back(encoded);
         }
