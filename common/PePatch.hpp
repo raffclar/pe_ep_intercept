71248c902ee2bb7d66bab577334bb6afadb5127e
Gavxn <gavxn@zanidrak.com>
2018-05-14 13:31:30 +0100

    Refs #overwrite-fix, Fix some styling issues with PePatch

--- common/PePatch.hpp
+++ common/PePatch.hpp
@@ -1,18 +1,24 @@
 #ifndef PE_EP_INTERCEPT_PEPATCH_HPP
 #define PE_EP_INTERCEPT_PEPATCH_HPP
 
+#include <utility>
+
 #include "Assembler.hpp"
 #include "PeFile.hpp"
+#include "PeStructs.hpp"
 
 namespace Interceptor {
+    using namespace Interceptor::RawHeaders;
+
     class PePatch {
     protected:
         static const uint32_t section_rights = scn_code | scn_mem_exe | scn_mem_read | scn_mem_write;
         Assembler assembler;
         PeFile file;
-        uint32_t Align(uint32_t num, uint32_t multiple);
+        uint32_t align(uint32_t num, uint32_t multiple);
+        virtual void addSection(const std::string &name) = 0;
     public:
-        PePatch(const Assembler &assembler, const PeFile &file) : assembler(assembler), file(file) {};
+        PePatch(const Assembler &assembler, PeFile file) : assembler(assembler), file(std::move(file)) {};
     };
 }
 
